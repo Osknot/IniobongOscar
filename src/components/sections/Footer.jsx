c407fe5b4a191e8a43b2d3808ea4503b8944e620
Osknot <inioscarcourse@gmail.com>
2026-02-03 23:24:47 +0100

    social links

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -13,19 +13,19 @@ export const Footer = () => {
 
     {/* Social Icons */}
     <div className="flex space-x-6">
-      <a href="https://www.tiktok.com/@yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
+      <a href="https://www.tiktok.com/@ini_oscar_" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
         <FaTiktok />
       </a>
-      <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
+      <a href="https://www.facebook.com/share/1GR21ik3i7/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
         <FaFacebookF />
       </a>
-      <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
+      <a href="https://www.linkedin.com/in/iniobongoscar" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
         <FaLinkedinIn />
       </a>
-      <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
+      <a href="https://www.instagram.com/ini__oscar?igsh=bTBtdmtnOG40Ymhr&utm_source=qr" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
         <FaInstagram />
       </a>
-      <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
+      <a href="hhttps://github.com/Osknot" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500 transition-colors text-xl">
         <FaGithub />
       </a>
     </div>
